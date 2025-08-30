6fdb86b8407728cbc1fe1c33facc15e8011fb59e
Ryan Coppolo <ryan@rockfruit.com>
2025-08-30 03:38:33 -0700

    Initial

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for testing."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """"""
+__email__ = "you@gmail.com"
+__version__ = "0.0.1"
+
+from .src.testing.nodes import NODE_CLASS_MAPPINGS
+from .src.testing.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
